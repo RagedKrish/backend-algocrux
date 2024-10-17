e28be2a8329822fa7bcc08eb8cf6661a7bfdb39e
Krishs21 <117849849+RagedKrish@users.noreply.github.com>
2024-10-18 00:47:46 +0530

    ..

--- server.go
+++ server.go
@@ -16,6 +16,12 @@ func corsMiddleware() gin.HandlerFunc {
         c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
         c.Writer.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
         c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+        
+        if c.Request.Method == "OPTIONS" {
+            c.AbortWithStatus(204)
+            return
+        }
+
         c.Next()
     }
 }
